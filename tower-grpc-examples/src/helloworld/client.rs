968d8e6158c335ba01238865276a0bd6c8aeae77
Ian P. Cooke <icooke@3redpartners.com>
2019-07-16 05:55:27 -0500

    failed attempt to refactor client.rs

--- tower-grpc-examples/src/helloworld/client.rs
+++ tower-grpc-examples/src/helloworld/client.rs
@@ -6,16 +6,14 @@ use tower_grpc::Request;
 use tower_hyper::{client, util};
 use tower_util::MakeService;
 
+use std::error::Error;
+
 pub mod hello_world {
     include!(concat!(env!("OUT_DIR"), "/helloworld.rs"));
 }
 
-pub fn main() {
-    let _ = ::env_logger::init();
-
-    let uri: http::Uri = format!("http://[::1]:50051").parse().unwrap();
-
-    let dst = Destination::try_from_uri(uri.clone()).unwrap();
+fn make_client(uri: http::Uri) -> Result<Box<dyn Future<Item=hello_world::client::Greeter<Box<dyn tower_grpc::codegen::client::grpc::GrpcService<(), ResponseBody=(), Future=(), Error=()>>>, Error=tower_grpc::Status> + Send>, Box<dyn Error>> {
+    let dst = Destination::try_from_uri(uri.clone())?;
     let connector = util::Connector::new(HttpConnector::new(4));
     let settings = client::Builder::new().http2_only(true).clone();
     let mut make_client = client::Connect::with_builder(connector, settings);
@@ -32,8 +30,17 @@ pub fn main() {
                 .unwrap();
 
             // Wait until the client is ready...
-            Greeter::new(conn).ready()
-        })
+            Greeter::new(Box::new(conn)).ready()
+        });
+    Ok(Box::new(say_hello))
+}
+
+pub fn main() -> Result<(), Box<dyn Error>> {
+    let _ = ::env_logger::init();
+
+    let uri: http::Uri = format!("http://[::1]:50051").parse()?;
+
+    let say_hello = make_client(uri)?
         .and_then(|mut client| {
             use crate::hello_world::HelloRequest;
 
@@ -50,4 +57,5 @@ pub fn main() {
         });
 
     tokio::run(say_hello);
+    Ok(())
 }
